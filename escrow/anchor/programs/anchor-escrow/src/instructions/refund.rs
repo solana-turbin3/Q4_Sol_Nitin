21c245df37b811feb993a6f12179fcc4b2ea3867
vnitin08 <nvermaofficial1@gmail.com>
2024-11-03 19:01:05 +0530

    add take

--- escrow/anchor/programs/anchor-escrow/src/instructions/refund.rs
+++ escrow/anchor/programs/anchor-escrow/src/instructions/refund.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 use anchor_spl::token_interface::{transfer_checked, close_account, Mint, TokenAccount, TokenInterface, TransferChecked, CloseAccount};
 
 
-use crate::state::{Escrow};
+use crate::state::Escrow;
 
 #[derive(Accounts)]
 pub struct Refund<'info> {
--- escrow/anchor/programs/anchor-escrow/src/lib.rs
+++ escrow/anchor/programs/anchor-escrow/src/lib.rs
@@ -11,11 +11,18 @@ declare_id!("6mgdkHJqFx5k8jhyiNs8PkfigeZ8CU6NdkUrsiytnE7g");
 pub mod anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn taker(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
